3a767ef9c33b1fff29aa2f655cf28758be70e4dd
James-Joslin <jamesjoslin@hotmail.co.uk>
2022-09-22 13:11:28 +0100

    Models folder structure

--- inference.py
+++ inference.py
@@ -11,7 +11,7 @@ EPOCHS = 250
 SEED = 7
 
 image_data = ext.getImageData(
-    train_dir = "./train", val_dir = "./val", test_dir = "./test"
+    train_dir = "./data/train", val_dir = "./data/val", test_dir = "./data/test"
 ).loadImageData(
     HEIGHT, WIDTH, DEPTH, BATCH_SIZE, SEED
 )
@@ -19,7 +19,7 @@ INDICES = image_data['test'].class_indices
 print(INDICES)
 
 model = ext.load_model(
-    save_dir = "./", model_name = "multiclass_image_classification"
+    save_dir = "./saved_models/", model_name = "multiclass_image_classification"
 )
 model.compile(
     optimizer = "Nadam",
